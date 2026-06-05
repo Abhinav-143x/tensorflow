mridul-sahu/xla

Add bounds checks to GetDim/SetDim in dnn.h.
PiperOrigin-RevId: 918520311

xla/stream_executor/dnn_test.cc
@@ -55,5 +55,14 @@ TEST(DnnTest, VersionInfoComparisonOperators) {
   }
 }
 
+TEST(DnnDeathTest, PoolingDescriptorSetDimOutOfBounds) {
+  EXPECT_DEATH(
+      {
+        dnn::PoolingDescriptor pool(1);
+        pool.set_window_height(1337);
+      },
+      "\\(1 vs\\. 1\\)");
+}
+
 }  // namespace
 }  // namespace stream_executor

Abhinav-143x/tensorflow

third_party/xla/xla/stream_executor/dnn_test.cc
@@ -56,6 +56,15 @@ TEST(DnnTest, VersionInfoComparisonOperators) {
   }
 }
 
+TEST(DnnTest, PoolingDescriptorSetDimOutOfBounds) {
+  EXPECT_DEATH(
+      {
+        dnn::PoolingDescriptor pool(1);
+        pool.set_window_height(1337);
+      },
+      "\\(1 vs\\. 1\\)");
+}
+
 TEST(DnnTest, ReorderDimsRankBelow2) {
   EXPECT_DEATH(dnn::ReorderDims({0}, dnn::DataLayout::kYXBatchDepth,
                                 dnn::DataLayout::kBatchYXDepth),